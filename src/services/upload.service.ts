4bdbc63f24a9d72828c83d8d75632f12062d246b
khris-xp <khrisbharmmano@gmail.com>
2023-10-18 18:26:16 +0700

    refac(type): add interface service

--- src/services/upload.service.ts
+++ src/services/upload.service.ts
@@ -1,11 +1,13 @@
+import { IResponse } from "../interfaces/response";
+import { IUploadService } from "../interfaces/service";
 import axiosInstance from "./api.service";
 
-export const uploadService = {
+export const uploadService: IUploadService = {
     uploadFileService: async (file: File) => {
         try {
             const formData = new FormData();
             formData.append('file', file);
-            const response = await axiosInstance.post('/predict', formData, {
+            const response: IResponse = await axiosInstance.post('/predict', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
